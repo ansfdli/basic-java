7058c0eff76f1ab9f3791e6c9ad4cadb1144a33b
ansfdli <anasfadli@outlook.com>
2022-05-11 16:33:31 +0700

    basic java

---
+++ src/TipeDataChar.java
@@ -0,0 +1,8 @@
+public class TipeDataChar {
+  public static void main(String[] args) {
+
+    char e = 'E';
+    char p = 'P';
+    char u = 'U';
+  }
+}
---
+++ src/TipeDataNumber.java
@@ -0,0 +1,19 @@
+public class TipeDataNumber {
+
+    public static void main(String[] args) {
+      byte iniByte = 100;
+      short iniShort = 1000;
+      int iniInt = 1000000000;
+      long iniLong = 1000000000;
+      long iniLong2 = 1000000000L;
+
+      float iniFloat = 10.10F;
+      double iniDouble = 10.10;
+
+      int decimalInt = 29;
+      int hexaDecimal = 0xFFFFF;
+      int binaryInt = 0b1010010;
+
+      int amount = 1_000_000_000;
+    }
+}
