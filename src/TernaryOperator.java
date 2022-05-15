dd2aa45f919bab1aca8bd0b47821cec7f6afdc9b
ansfdli <anasfadli@outlook.com>
2022-05-15 20:58:25 +0700

    basic java

---
+++ src/TernaryOperator.java
@@ -0,0 +1,20 @@
+public class TernaryOperator {
+  public static void main(String[] args) {
+
+    var nilai = 75;
+//    String pesan;
+
+//    if(nilai >= 75){
+//      pesan = "lulus";
+//    } else {
+//      pesan = "coba lagi";
+//    }
+//    System.out.println(pesan);
+
+
+    //ternary operator
+    String pesan = nilai >= 75 ? "lulus" : "coba lagi";
+
+    System.out.println(pesan);
+  }
+}
